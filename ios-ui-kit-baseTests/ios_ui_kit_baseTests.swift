bca2658ab5e82f5a5dbac1976a2d9fd910c956a3
Adam Zarn <ajzarn@gmail.com>
2021-11-02 12:54:23 -0500

    Create project

---
+++ ios-ui-kit-baseTests/ios_ui_kit_baseTests.swift
@@ -0,0 +1,33 @@
+//
+//  ios_ui_kit_baseTests.swift
+//  ios-ui-kit-baseTests
+//
+//  Created by Adam Zarn on 11/2/21.
+//
+
+import XCTest
+@testable import ios_ui_kit_base
+
+class ios_ui_kit_baseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
